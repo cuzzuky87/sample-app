ae33f345c5a3f8fcdb85fd9d035adc58a7559287
cuzzuky87 <kazu_l_k@yahoo.co.jp>
2020-01-31 23:21:18 +0900

    kalendar試してみた

--- src/components/MyCalendar.vue
+++ src/components/MyCalendar.vue
@@ -1,50 +1,47 @@
-<template>
-  <v-row>
-    <v-col>
-      <v-sheet height="600">
-        <v-calendar
-          ref="calendar"
-          :now="today"
-          :value="today"
-          :events="events"
-          color="primary"
-          type="week"
-        ></v-calendar>
-      </v-sheet>
-    </v-col>
-  </v-row>
-</template>
-
 <script>
-//import api from "../store";
-
-export default {
-  data: () => ({
-    today: new Date().toISOString().substr(0, 10),
-    events: []
-  }),
-  mounted() {
-    this.getEvents();
-  },
-  methods: {
-    getEvents() {
-      const events = [];
-      fetch("http://127.0.0.1:8000/api/v1/events/")
-        .then(res => {
-          if (res.ok) {
-            return res.json();
-          } else {
-            console.log("ng");
-          }
-        })
-        .then(res => {
-          res.forEach(e => {
-            events.push(e);
-          });
-        })
-        .catch(err => console.log("error in post" + err));
-      this.events = events;
-    }
-  }
-};
+//export default {
+//  data: () => ({
+//    events: [],
+//    calendar_settings: {
+//      style: "material_design",
+//      view_type: "week",
+//      cell_height: 15,
+//      scrollToNow: false,
+//      current_day: new Date(),
+//      read_only: false,
+//      day_starts_at: 8,
+//      day_ends_at: 22,
+//      overlap: false,
+//      hide_dates: [],
+//      hide_days: [],
+//      past_event_creation: false
+//    }
+//  }),
+//  mounted() {
+//    this.getEvents();
+//  },
+//  methods: {
+//    getEvents() {
+//      const events = [];
+//      fetch("http://127.0.0.1/api/v1/events/")
+//        .then(res => {
+//          if (res.ok) {
+//            return res.json();
+//          } else {
+//            console.log("ng");
+//          }
+//        })
+//        .then(res => {
+//          res.forEach(e => {
+//            events.push;
+//          });
+//        })
+//        .catch(err => console.log(err));
+//
+//      this.events = events;
+//    }
+//  }
+//};
+//
 </script>
+//
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,55 @@
 <template>
   <div class="home">
-    <MyCalendar />
+    <Kalendar :configuration="calendar_settings" :events="events" />
   </div>
 </template>
 
 <script>
-import MyCalendar from "../components/MyCalendar";
+import { Kalendar } from "kalendar-vue";
+
 export default {
   name: "HomePage",
-  components: { MyCalendar },
-  data: () => ({})
+  components: { Kalendar },
+  data: () => ({
+    events: [],
+    calendar_settings: {
+      style: "material_design",
+      view_type: "week",
+      cell_height: 15,
+      scrollToNow: false,
+      current_day: new Date(),
+      read_only: false,
+      day_starts_at: 8,
+      day_ends_at: 22,
+      overlap: false,
+      hide_dates: [],
+      hide_days: [],
+      past_event_creation: false
+    }
+  }),
+  mounted() {
+    this.$kalendar.getEvents();
+  },
+  methods: {
+    getEvents() {
+      const events = [];
+      fetch("http://127.0.0.1/api/v1/events/")
+        .then(res => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            console.log("ng");
+          }
+        })
+        .then(res => {
+          res.forEach(e => {
+            events.push(e);
+          });
+        })
+        .catch(err => console.log(err));
+
+      this.events = events;
+    }
+  }
 };
 </script>
