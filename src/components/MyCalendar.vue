7682a74a966b9f803e3d4b334dc1dd3f39456cca
cuzzuky87 <kazu_l_k@yahoo.co.jp>
2020-02-02 23:33:14 +0900

    wip: fetch events

--- src/components/MyCalendar.vue
+++ src/components/MyCalendar.vue
@@ -63,6 +63,26 @@ export default {
   }),
   mounted() {
     this.$refs.calendar.scrollToTime("08:00");
+    this.getEvents();
+  },
+  methods: {
+    getEvents() {
+      const events = [];
+      fetch("http://127.0.0.1:8000/api/v1/events/")
+        .then(res => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            console.log("ng");
+          }
+        })
+        .then(res => {
+          res.forEach(e => {
+            events.push(e);
+          });
+        })
+        .catch(err => console.log("error in post" + err));
+    }
   }
 };
 </script>
